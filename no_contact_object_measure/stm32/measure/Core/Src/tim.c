8fec3c911eabfac1c8e5c777f26e6ca332ab6798
RaymondMeng <2391530014@qq.com>
2021-10-31 16:18:53 +0800

    更新项目进度

--- no_contact_object_measure/stm32/measure/Core/Src/tim.c
+++ no_contact_object_measure/stm32/measure/Core/Src/tim.c
@@ -150,7 +150,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* tim_baseHandle)
     __HAL_RCC_TIM1_CLK_ENABLE();
 
     /* TIM1 interrupt Init */
-    HAL_NVIC_SetPriority(TIM1_UP_TIM10_IRQn, 3, 1);
+    HAL_NVIC_SetPriority(TIM1_UP_TIM10_IRQn, 3, 0);
     HAL_NVIC_EnableIRQ(TIM1_UP_TIM10_IRQn);
   /* USER CODE BEGIN TIM1_MspInit 1 */
 
