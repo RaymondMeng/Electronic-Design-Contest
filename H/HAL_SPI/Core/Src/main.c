9b7a5ec1fcf07f6ec10db3b8a5ae919d2148b01c
RaymondMeng <2391530014@qq.com>
2021-11-07 14:08:39 +0800

    上传了最新版

--- H/HAL_SPI/Core/Src/main.c
+++ H/HAL_SPI/Core/Src/main.c
@@ -37,9 +37,9 @@
 /* USER CODE BEGIN PD */
 #define CURRENT_DAT_TIMES 256
 #define DATA_RATIO 94
-#define TYPE_NUM 6
+#define TYPE_NUM 8
 #define FEATURE_NUM 5
-#define buff_SIZE 3
+#define buff_SIZE 32
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -52,23 +52,40 @@
 /* USER CODE BEGIN PV */
 //A相电流有效值 A相有功功率 A相无功功率 A相功率因数 A相电流与电压相角
 int feature_flash[TYPE_NUM][FEATURE_NUM] = {
-     500,    5,        27,      2348810,   -524472,   //没有插任何电器
-     8000,   649,      800,     5321192,   397179,   //灯带
-     11174,  -1440,    4197,    -2739561,  637365,   //电风扇
-     2245,   120,      792,     1288176,   371386,     //节能灯
-     2891,   19,       367,     490698,    450987,      //树莓派
-     799341, 108485,   319306,  2695306,   366848,       //热水壶
+     1082,    9,        26,      0,   -524344,   //没有插任何电器
+     7555,   636,      265,     0,   0,   //灯带                   1         1
+     10235,  -1476,    4110,    0,  0,   //电风扇                  2         2
+     1320,   122,      774,     0,   0,     //节能灯               4         3
+     2254,   12,       368,     0,    0,      //树莓派             8         4
+     528,    154,       450,   0,     0,                   //盖佬的电阻        16
+     1749,    338 ,     453,   0,     0,   //自制1号                                32
+     860000, 115234, 343953,   0,   0,       //电热器             64
       
 };
-float distance[TYPE_NUM] = {0};   
-float temp_long=0;
-int HT7038[FEATURE_NUM] = {0};
-float temp_f32 = 0.0;
-uint32_t temp_u32 = 0;
-uint32_t class_array[buff_SIZE] = {0};
-uint32_t class_keep = 0;
-unsigned int temp;
+
+
+//有关计算类别的变量
+/********************************/
 uint32_t count;
+float distance = 0; 
+int class_min;
+int HT7038[FEATURE_NUM] = {0};
+//有功 无功
+int CalcFeature[4] = {0};
+float temp_f32 = 0.0; 
+uint32_t temp_u32 = 0; 
+long int temp_long = 0; 
+
+//均值
+int Feature_Buffer[3][buff_SIZE] = {0};
+int average[4] = {0};
+uint32_t buff_count = 0;
+/********************************/
+
+unsigned int temp;
+
+
+int cnt;
 
 int tx_buf[10] = {0};
 int cnt;
@@ -169,142 +186,138 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
-    //temp = att7053_Read(0x10);
-    int temp1 = 0;
-    temp_u32 = 0;
-	  temp_f32 = 0.0;
-    if(!mode)
-    {
-    for(cnt=0;cnt<TYPE_NUM;cnt++)
-        distance[cnt]=0;
-    CurrentDataReadFun();
+      temp_u32 = 0;
+      temp_f32 = 0.0;
     
-    //计算当前特征与flash特征距离
-    for(i = 0;i<TYPE_NUM;i++){
-      for(j = 0;j<FEATURE_NUM;j++){
-        if(j==3)
-            temp_f32 = pow(feature_flash[i][j]/10000 - HT7038[j]/10000,2);
-        else
-            temp_f32 = pow(feature_flash[i][j]/1000 - HT7038[j]/1000,2);
-        distance[i] += temp_f32;
-        printf("%f ", temp_f32);
+      //读取特征量到HT7038
+      CurrentDataReadFun();
+        
+      //有功无功均值
+      for(i = 0;i < 3;i++){
+          temp_long = 0;
+          for(j = 0; j < buff_SIZE;j++){
+              temp_long += Feature_Buffer[i][j];
+          }
+          temp_u32 = temp_long/buff_SIZE;
+          average[i] = temp_u32;
+          printf("%d ",temp_u32);
       }
-      printf("dist:%f \r\n ",distance[i]);
-    }
-    printf("\r\n");
-    
-      //寻找距离最小的类别
-      temp_f32 = distance[0];
-      for(i = 0;i<TYPE_NUM;i++){
-        if(distance[i] < temp_f32){
-          temp_f32 = distance[i];
-          temp_u32 = i;
-        }
-      }
-      
-	  //发送最小的类别
+      printf("\r\n");
+      //识别模式
+      if(!mode){
 
-      j = 0;
-      //判断数组里是否都是一样的 更新keep数组
-      for(i=0;i<buff_SIZE-1;i++){
-        if(class_array[i] != class_array[i+1])
-             j++;
-        class_array[i] = class_array[i+1];
+          //初始化参数
+          distance = pow(average[0]/10,2) + pow(average[1]/10,2); //+ pow(average[2]/10,2);
+          //printf("0: %f ",distance);
+          class_min = 0;     
+          //根据有功无功建立叠加表
+          for(i = 1;i < (1 <<(TYPE_NUM-1));i++){
+              CalcFeature[0] =
+                  feature_flash[1][1] * (i 	   &0x01)   +
+                  feature_flash[2][1] * ((i>>1)&0x01) +
+                  feature_flash[3][1] * ((i>>2)&0x01) +
+                  feature_flash[4][1] * ((i>>3)&0x01) +
+                  feature_flash[5][1] * ((i>>4)&0x01) +
+                  feature_flash[6][1] * ((i>>5)&0x01) +
+                  feature_flash[7][1] * ((i>>6)&0x01) ;
+            
+              CalcFeature[1] =
+                  feature_flash[1][2] * (i 	 &0x01)   +
+                  feature_flash[2][2] * ((i>>1)&0x01) +
+                  feature_flash[3][2] * ((i>>2)&0x01) +
+                  feature_flash[4][2] * ((i>>3)&0x01) +
+                  feature_flash[5][2] * ((i>>4)&0x01) +
+                  feature_flash[6][2] * ((i>>5)&0x01) +
+                  feature_flash[7][2] * ((i>>6)&0x01) ;
+            
+              CalcFeature[2] =
+                  feature_flash[1][2] * (i 	 &0x01)   +
+                  feature_flash[2][2] * ((i>>1)&0x01) +
+                  feature_flash[3][2] * ((i>>2)&0x01) +
+                  feature_flash[4][2] * ((i>>3)&0x01) +
+                  feature_flash[5][2] * ((i>>4)&0x01) +
+                  feature_flash[6][2] * ((i>>5)&0x01) +
+                  feature_flash[7][2] * ((i>>6)&0x01) ;
+          //最小欧拉距离
+              temp_f32 = pow(CalcFeature[0]/10 - average[0]/10,2) + pow(CalcFeature[1]/10 - average[1]/10,2); //+ pow(CalcFeature[2]/10 - average[2]/10,2);
+              //printf("%d: %d %d %f ",i,CalcFeature[0],CalcFeature[1],temp_f32);
+              if(temp_f32 < distance){
+                  distance = temp_f32;
+//                  if(average[0]>110000 && average[1]>340000)
+//                    i = 7;
+                  class_min = i;
+                  if(average[2] > 780000 && average[2] < 800000)
+                    class_min = 64;
+              }
+          }
+
+          printf("class: %d\r\n",class_min);
+          printf("phase: %f\r\n",(float)HT7038[4]*0.000171661);
+          printf("line freq: %f\r\n",(float)HT7038[3]/8192.0);
+          
+          //uartx_printf(huart2, "t0.txt=\"用电器%d\"", class_min);
+          //1234567
+          uartx_printf(huart2, "t0.txt=\"");
+          for(i = 1;i < TYPE_NUM;i++){
+              if(class_min&(0x01<<(i-1))){
+                  uartx_printf(huart2, "用电器%d ", i);        
+              }             
+          }
+          uartx_printf(huart2, "\"", i);
+          HAL_UART_Transmit(&huart2,end,3,0xffff);
+          
+          uartx_printf(huart2, "n1.val=%d", tx_buf[2]);
+          HAL_UART_Transmit(&huart2,end,3,0xffff);
+          
+          uartx_printf(huart2, "n2.val=%d", (int)HT7038[4]*0.000171661);
+          HAL_UART_Transmit(&huart2,end,3,0xffff);
       }
-      class_array[buff_SIZE-1] = temp_u32;
-      if(j==0)
-        class_keep = temp_u32;
-      printf("class: %d\r\n",class_keep);
       
-      uartx_printf(huart2, "t0.txt=\"用电器%d\"", class_keep);
-      HAL_UART_Transmit(&huart2,end,3,0xffff);
-      
-      uartx_printf(huart2, "n1.val=%d", tx_buf[2]);
-      HAL_UART_Transmit(&huart2,end,3,0xffff);
-      
-      uartx_printf(huart2, "n2.val=%d", tx_buf[6]);
-      HAL_UART_Transmit(&huart2,end,3,0xffff);
-    }
       
       /*学习模式*/
       if(mode)
       {
-          memset(feature_flash, 0x00, sizeof(feature_flash));
-          HAL_Delay(100); //等待稳定后开始测量
-          /*数据采样后平滑滤波后平均作为特征值*/
-          for(i = 0; i < 10; i++)
-          {
-              temp1 = att7053_Read(0x01); //A相有功功率
-              if(temp1 > 8388608) yougong_p = temp1 - 16777216;
-              else yougong_p = temp1;
-              yougong_sum += yougong_p * 10;
-              yougong_sum -= yougong_avg;
-              yougong_avg = yougong_sum * 1.0 / 5;
-              yougong_final = yougong_avg;
-              yougong_p = 0;
-              yougong_s += yougong_final;     
-            
-              temp1 = att7053_Read(0x01); //A相无功功率
-              if(temp1 > 8388608) wugong_p = temp1 - 16777216;
-              else wugong_p = temp1;
-              wugong_sum += wugong_p * 10;
-              wugong_sum -= wugong_avg;
-              wugong_avg = wugong_sum * 1.0 / 5;
-              wugong_final = wugong_avg;
-              wugong_p = 0;
-              wugong_s += wugong_final;
-            
-              temp1 = att7053_Read(0x01); //A相电流有效值
-              if(temp1 > 8388608) youxiao_c = temp1 - 16777216;
-              else youxiao_c = temp1;
-              youxiao_sum += youxiao_c * 10;
-              youxiao_sum -= youxiao_avg;
-              youxiao_avg = youxiao_sum * 1.0 / 5;
-              youxiao_final = youxiao_avg;
-              youxiao_c = 0;
-              youxiao_s += youxiao_final;
-              
-              temp1 = att7053_Read(0x01); //A相功率因数
-              if(temp1 > 8388608) yinshu = temp1 - 16777216;
-              else yinshu = temp1;
-              yinshu_sum += yinshu * 10;
-              yinshu_sum -= yinshu_avg;
-              yinshu_avg = yinshu_sum * 1.0 / 5;
-              yinshu_final = yinshu_avg;
-              yinshu = 0;
-              yinshu_s += yinshu_final;
-              
-              temp1 = att7053_Read(0x01); //A相电流与电压相角
-              if(temp1 > 8388608) xiangjiao = temp1 - 16777216;
-              else xiangjiao = temp1;
-              xiangjiao_sum += xiangjiao * 10;
-              xiangjiao_sum -= xiangjiao_avg;
-              xiangjiao_avg = xiangjiao_sum * 1.0 / 5;
-              xiangjiao_final = xiangjiao_avg;
-              xiangjiao = 0;
-              xiangjiao_s += xiangjiao_final;
+          if(mat_cnt==0){
+            //第0行是底噪  不清空
+              feature_flash[mat_cnt][1] += average[0];
+              feature_flash[mat_cnt][2] += average[1];
+            //清空
+              for(i = 1;i < TYPE_NUM;i++){
+                  for(j = 0; j < FEATURE_NUM;j++){
+                      feature_flash[i][j] = 0;
+                  }
+              }              
           }
-          yougong_p = yougong_s / 10;  //将这个值写入表中
-          wugong_p = wugong_s / 10;   //将这个值写入表中
-          xiangjiao = xiangjiao_s / 10;
-          youxiao_c = youxiao_s / 10;
-          yinshu = yinshu / 10;
-          
-          feature_flash[mat_cnt-1][0] = yougong_p;
-          feature_flash[mat_cnt-1][1] = wugong_p;
-          feature_flash[mat_cnt-1][2] = xiangjiao;
-          feature_flash[mat_cnt-1][3] = youxiao_c;
-          feature_flash[mat_cnt-1][4] = yinshu;
-          
-          uartx_printf(huart2, "t8.txt=\"学习完成...\"");
+          else{
+            //替换
+              feature_flash[mat_cnt][1] = average[0];
+              feature_flash[mat_cnt][2] = average[1];
+          }
+
+          mat_cnt++;
+          if(mat_cnt>=TYPE_NUM)
+                mat_cnt = 0;
+        
+          uartx_printf(huart2, "t8.txt=\"学习完成\"");
           HAL_UART_Transmit(&huart2,end,3,0xffff);
-          mode = 0;   
+          printf("学习完成\r\n");
+          
+          //打印出来看
+          for(i = 0;i < TYPE_NUM;i++){
+              for(j = 0; j < FEATURE_NUM;j++){
+                  printf("%d ",feature_flash[i][j]);
+              }
+              printf("\r\n");
+          }
+          
+          mode = 0;
+          HAL_Delay(2000);
       }
           
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    HAL_Delay(800);
+    HAL_Delay(80);
   }
   /* USER CODE END 3 */
 }
@@ -361,39 +374,46 @@ void CurrentDataReadFun()
   tx_buf[0] = 0xff;
 	tx_buf[1] = att7053_Read(0x0D); //A相电压
   
-	//currentDataBuf[1] += att7053_Read(0x12); //C相电流有效值
+
 	tx_buf[2] = att7053_Read(0x10); //A相电流有效值
-  HT7038[0] = tx_buf[2];
-	//currentDataBuf[5] += att7053_Read(0x10); //A相电流有效值
+  HT7038[0] = tx_buf[2] - feature_flash[0][0];  //减去电流偏置
+
   temp = att7053_Read(0x01); //A相有功功率
   if(temp>8388608) tx_buf[3] = temp - 16777216;
   else tx_buf[3] = temp;
-  HT7038[1] = tx_buf[3];
+  HT7038[1] = tx_buf[3] - feature_flash[0][1];  //减去有功偏置
   
   temp = att7053_Read(0x05); //A相无功功率
   if(temp>8388608) tx_buf[4] = temp - 16777216;
   else tx_buf[4] = temp;
-  HT7038[2] = tx_buf[4];
-  
-  //tx_buf[5] = att7053_Read(POWERP1); //B相视在功率
-  temp = att7053_Read(0x14); //A相功率因数
-  if(temp>8388608) tx_buf[5] = temp - 16777216;
-  else tx_buf[5] = temp;
+  HT7038[2] = tx_buf[4] - feature_flash[0][2];  //减去无功偏置
+
+  temp = att7053_Read(0x1c); //A相线频率
+  //if(temp>8388608) tx_buf[5] = temp - 16777216;
+  //else tx_buf[5] = temp;
+  tx_buf[5] = temp;
   HT7038[3] = tx_buf[5];
   
   temp = att7053_Read(0x18);  //A相电流与电压相角
   if(temp>1048576) tx_buf[6] = temp - 16777216;
   else tx_buf[6] = temp;
-  HT7038[4] = tx_buf[6];
+  HT7038[4] = tx_buf[6] - feature_flash[0][4];//减去相角偏置
   
   tx_buf[7] = 0xfe;
 
 
+  Feature_Buffer[0][buff_count] = HT7038[1];
+  Feature_Buffer[1][buff_count] = HT7038[2];
+  Feature_Buffer[2][buff_count] = HT7038[0]; //电流有效值
+  buff_count++;
+  if(buff_count>=buff_SIZE)
+      buff_count = 0;
+  
   printf("\r\n1.%d\r\n", tx_buf[2]);
-  printf("\r\n2.%d\r\n", tx_buf[3]);
-  printf("\r\n3.%d\r\n", tx_buf[4]);
-  printf("\r\n4.%d\r\n", tx_buf[5]);
-  printf("\r\n5.%d\r\n", tx_buf[6]);
+//  printf("\r\n2.%d\r\n", tx_buf[3]);
+//  printf("\r\n3.%d\r\n", tx_buf[4]);
+//  printf("\r\n4.%d\r\n", tx_buf[5]);
+//  printf("\r\n5.%d\r\n", tx_buf[6]);
 
 }
 
@@ -403,10 +423,11 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
    {
       if(UART2_RxBuffer == 0x01)
       {
+          uartx_printf(huart2, "t8.txt=\"开始清理全部特征参数\"");
+          HAL_UART_Transmit(&huart2,end,3,0xffff);
           uartx_printf(huart2, "t8.txt=\"学习中...\"");
           HAL_UART_Transmit(&huart2,end,3,0xffff);
           mode = 1; //模式切换
-          mat_cnt++;
       }
       HAL_UART_Receive_IT(&huart2, (uint8_t*)&UART2_RxBuffer, 1);      
    }
